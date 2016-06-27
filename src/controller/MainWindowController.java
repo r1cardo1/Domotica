aceb0d7f33a085fd47e72ab21c1db7197d17af63
Ricardo Marcano <Ricardo Marcano@RicardoMarcano>
2016-06-27 01:26:33 -0400

    Agregado renderizado de texturas, panel lateral simplificado de lineas en la clase principal

--- src/controller/MainWindowController.java
+++ src/controller/MainWindowController.java
@@ -5,17 +5,21 @@
  */
 package controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.MeshView;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
 /**
@@ -24,21 +28,33 @@ import javafx.util.Duration;
  * @author Ricardo Marcano
  */
 public class MainWindowController implements Initializable {
+    
     @FXML
     Group root;
-    Group home;
     int i=0;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        try {
-            home=FXMLLoader.load(getClass().getResource("/fxml/Drawing1.fxml"));
-        } catch (IOException ex) {}
-        root.getChildren().add(home);
-        home.setTranslateX(100);
-        home.setTranslateY(250);
-        home.setFocusTraversable(true);
-        root.setOnKeyPressed((KeyEvent evt)->{
+        
+    }
+    
+    public void openDoor(Node n){
+        RotateTransition rt = new RotateTransition();
+        rt.setNode(n);
+        rt.setByAngle(90);
+        rt.setDuration(Duration.millis(1000));
+        rt.setAutoReverse(true);
+        rt.setCycleCount(2);
+        rt.play();
+    }
+    
+    public void closeDoor(Node n){
+        
+    }
+    
+    @FXML
+    public void keyPres(KeyEvent evt){
+        root.setRotationAxis(Rotate.Z_AXIS);
             if(evt.getCode()==KeyCode.UP){
                 i++;
                 System.out.println(i);
@@ -48,28 +64,19 @@ public class MainWindowController implements Initializable {
                 System.out.println(i);
             }
             if(evt.getCode()==KeyCode.LEFT){
-                openDoor(home.getChildren().get(i));
+                root.setRotate(root.getRotate()-1);
             }
             if(evt.getCode()==KeyCode.RIGHT){
-                openDoor(home.getChildren().get(i));
-            }
-            
-        });
+                root.setRotate(root.getRotate()+1);
+            }            
+        }    
+    
+    @FXML
+    public void mouse(MouseEvent evt){
+        PickResult res = evt.getPickResult();
+            System.out.println(res);
+            MeshView m = (MeshView) res.getIntersectedNode();
+            m.setMaterial(new PhongMaterial(Color.RED));
     }    
-    
-    public void openDoor(Node n){
-        RotateTransition rt = new RotateTransition();
-        rt.setNode(n);
-        rt.setByAngle(90);
-        rt.setDuration(Duration.millis(1000));
-        rt.setAutoReverse(true);
-        rt.setCycleCount(80);
-        rt.play();
-    }
-    
-    public void closeDoor(Node n){
-        
-    }
-    
     
 }
